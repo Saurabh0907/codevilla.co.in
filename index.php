1f35a4fa68d01defbea88124b955b0234e81a1cf
Saurabh Garg <gargsaurabh1993@gmail.com>
2014-12-18 00:38:55 +0530

    Added Features

---
+++ index.php
@@ -0,0 +1,198 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="header.css">
+
+<link rel="stylesheet" type="text/css" href="middle2.css">
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script>
+
+
+
+
+
+
+$(document).ready(function() { //When the document loads
+  $("#aboutUs").click(function(){
+    $(window).scrollTop($("#aboutus1").offset().top); //scroll to div with container as ID.
+    return false; //Prevent Default and event bubbling.
+  });
+});
+
+$(document).ready(function() { //When the document loads
+  $("#features").click(function(){
+    $(window).scrollTop($("#Features").offset().top); //scroll to div with container as ID.
+    return false; //Prevent Default and event bubbling.
+  });
+});
+
+</script>
+<title>CodeVilla - A House where you can code.</title>
+</head>
+
+
+
+
+
+
+
+
+<body bgcolor="#333333">
+
+<div id="MathJax_Message" style="display: none;"></div>
+
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner"> 
+    <div class="container"> 
+    
+    <a href="" onclick="window.location.reload();"><i class="brand"><img src="img/c.png" style="height:45px"></i></a>
+
+        
+      <div class="nav-collapse collapse pull-right">
+        <ul class="nav" style="padding-top:8px">
+        <li class=""><a href=" " id="features">Features</a></li>
+        <li><a  href =" " id="aboutUs">About Us</a></li>
+        
+       
+
+        </ul>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+
+<div class="jumbotron masthead marketing" style="background: linear-gradient(rgba(85, 94, 10, 0.2), rgba(93, 89, 10, 0.2)), url('img/fabric.png');background-position: center; ">
+  <div class="container" style="padding-left:100px">
+
+    <div class="row">
+      <div class="offset1 span6" style="text-align:left;">
+
+        <center><h2>A House Where You Can Code & Write Future Technology Together</h2><br>
+        <span class="marketing-byline">CodeVilla  is the <span>collaborative</span> platform for Coding. Write and manage your codes at one place.</span>
+        </center>
+
+        <center>
+          <a class="btn btn-large btn-primary btn-start-now" type="submit" href="http://codevilla.co.in/editor/">
+            <i class="icon-pencil"></i>Get Started&nbsp&nbsp</a>
+        </form> 
+        </center>
+
+      </div>
+      <div class="span5">
+        <center>
+</center>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="marketing" id="Features" style="height:100%;margin-top:-5%;">
+  <hr class="soften">
+
+  <center>
+    <div style="max-width:750px; margin-bottom:70px;"><h2>A NEW WAY TO CODE - ANYTIME, ANYWHERE</h2></div>
+  </center>
+
+    <div class="row-fluid" style="height:300">
+      <div class="span4">
+        <img alt="Code and write anytime" class="marketing-img" src="img/editor.png">
+        <h2 class="marketing-title">Write</h2>
+        <p>You can write code anywhere,anytime and save it in your CodeVilla account or you can save it in Dropbox.</p>
+      </div>
+      <div class="span4">
+        <img alt="Compiler" class="marketing-img" src="img/setting.png">
+        <h2 class="marketing-title">Compile</h2>
+        <p>You can compile your code anytime from anywhere, <strong>just one click needed</strong>.</p>
+      </div>
+      <div class="span4">
+        <img alt="Save to cloud" class="marketing-img" src="img/drop.png">
+        <h2 class="marketing-title">Save</h2>
+        <p>You can fetch the documents saved in Dropbox and edit them in CodeVilla, and then save back as well.</p>
+      </div>         
+    </div>
+</div>
+
+
+
+
+
+<center>
+
+  <h2>THE CODE VILLA TEAM</h2><br>
+  </center>
+  
+
+
+<div id="aboutus1" style="margin-left:25% ;color:#D6D6D6">
+
+<center>
+
+  
+
+<div class="row-fluid" style="color:#D6D6D6">
+  <ul class="thumbnails">
+
+    <li class="span3 contact-thumb-li">
+      <div class="thumbnail">
+        <img src="img/c.png" style="height:25px;opacity:0.1;">
+        <div class="caption">
+          <address>
+            <strong>GAURAV PANDVIA</strong>
+			<img src="img/gp.jpg" alt="Gaurav Pandvia"/><br><br>
+			<a target="_blank" href="https://www.facebook.com/gaurav.pandvia"><img src="img/fb.png" style="height:25px;width:25px;"/></a>&nbsp &nbsp
+            <a target="_blank" href="https://www.linkedin.com/profile/view?id=317368964&authType=NAME_SEARCH&authToken=xw0Y&locale=en_US&srchid=1442910601418754652729&srchindex=1&srchtotal=3&trk=vsrp_people_res_name&trkInfo=VSRPsearchId%3A1442910601418754652729%2CVSRPtargetId%3A317368964%2CVSRPcmpt%3Aprimary"><img src="img/in.png" style="height:25px;width:25px;"/></a>
+            
+              </address>
+        </div>
+      </div>
+    </li>
+
+    <li class="span3 contact-thumb-li">
+      <div class="thumbnail">
+        <img src="img/c.png" style="height:25px;opacity:0.1;">
+        <div class="caption">
+          <address>
+            <strong>SAURABH GARG</strong>
+            <img src="img/sg.jpg" alt="Saurabh Garg"/><br><br>
+            <a target="_blank" href="https://www.facebook.com/SaurabhGarg09"><img src="img/fb.png" style="height:25px;width:25px;"/></a>&nbsp &nbsp
+            <a target="_blank" href="https://github.com/Saurabh0907"><img src="img/git.png" style="height:25px;width:25px;"/></a>&nbsp &nbsp
+            <a target="_blank" href="http://in.linkedin.com/in/saurabhgarg09"><img src="img/in.png" style="height:25px;width:25px;"/></a>
+            
+          </address>
+        </div>
+      </div>
+    </li>  
+
+    <li class="span3 contact-thumb-li">
+      <div class="thumbnail">
+        <img src="img/c.png" style="height:25px;opacity:0.1;">
+        <div class="caption">
+          <address>
+            <strong>SHIKHER MISHRA</strong><br>
+			<img src="img/sm1.jpg" alt="Sihkher Mishra"/><br><br>
+			<a target="_blank" href="https://www.facebook.com/shikher.mishra.1"><img src="img/fb.png" style="height:25px;width:25px;"/></a>&nbsp &nbsp
+            <a target="_blank" href="https://www.linkedin.com/profile/view?id=176886082&authType=name&authToken=scL9&trk=prof-proj-cc-name"><img src="img/in.png" style="height:25px;width:25px;"/></a>
+            
+          </address>
+        </div>
+      </div>
+    </li>  
+
+            
+
+
+</ul></div> 
+</center>
+</div>
+
+
+
+
+
+
+</body>
+</html>
